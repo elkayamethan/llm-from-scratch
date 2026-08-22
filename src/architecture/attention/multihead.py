979bce96e0cba4e4d4a992b1f9a4e852a29dd60d
Ethan Elkayam <ethanelkaim17@gmail.com>
2026-08-22 06:25:51 +0300

    Implemented Multi-Head Attention

--- src/architecture/attention/multihead.py
+++ src/architecture/attention/multihead.py
@@ -1,27 +1,33 @@
 import torch
 from torch import nn, Tensor
 
-# NOTE: Has been made obsolete by MultiHeadAttention
-class CausalAttention(nn.Module):
-    """A single-head causal attention module with dropout"""
+class MultiHeadAttention(nn.Module):
+    """A multi-head causal attention module with dropout and a final linear projection layer"""
 
     def __init__(
             self,
             d_in: int,
             d_out: int,
+            num_heads: int,
             context_length: int,
             *,
             dropout: float = 0.5,
             kqv_bias: bool = False,
     ) -> None:
         super().__init__() 
+        if d_out % num_heads != 0:
+            raise ValueError("d_out must be divisible by num_heads")
+
         self.d_in = d_in
         self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
         self.context_length = context_length
 
         self.W_k = nn.Linear(in_features=d_in, out_features=d_out, bias=kqv_bias)
         self.W_q = nn.Linear(in_features=d_in, out_features=d_out, bias=kqv_bias)
         self.W_v = nn.Linear(in_features=d_in, out_features=d_out, bias=kqv_bias)
+        self.final_proj = nn.Linear(in_features=d_out, out_features=d_out)
 
         self.dropout = nn.Dropout(p=dropout)
 
@@ -32,7 +38,7 @@ class CausalAttention(nn.Module):
         )
 
     def forward(self, x: Tensor) -> Tensor:
-        num_tokens = x.shape[1]
+        batch_size, num_tokens, _ = x.shape
         if num_tokens > self.context_length:
             raise ValueError("num_tokens cannot exceed context_length")
 
@@ -40,15 +46,22 @@ class CausalAttention(nn.Module):
         queries: Tensor = self.W_q(x)
         values: Tensor = self.W_v(x)
 
-        attention_scores = queries @ keys.transpose(1, 2)
+        keys = keys.view(batch_size, num_tokens, self.num_heads, self.head_dim).transpose(1,2)
+        queries = queries.view(batch_size, num_tokens, self.num_heads, self.head_dim).transpose(1,2)
+        values = values.view(batch_size, num_tokens, self.num_heads, self.head_dim).transpose(1,2)
+
+        attention_scores = queries @ keys.transpose(2, 3)
         attention_scores_masked = attention_scores.masked_fill(
             mask=self.mask[:num_tokens, :num_tokens], 
             value=-torch.inf,
         )
         attention_weights = torch.softmax(
-            attention_scores_masked / self.d_out**0.5,
+            attention_scores_masked / self.head_dim**0.5,
             dim=-1,
         )
         attention_weights = self.dropout(attention_weights)
 
-        return attention_weights @ values
+        context_vectors = (attention_weights @ values).transpose(1,2)
+        context_vectors = context_vectors.reshape(batch_size, num_tokens, self.d_out)
+
+        return self.final_proj(context_vectors)
