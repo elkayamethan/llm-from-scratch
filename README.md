66eb88526e8fc3092ff936d45b3d01cb9b8547fa
Ethan Elkayam <ethanelkaim17@gmail.com>
2026-08-22 04:51:00 +0300

    Implemented Causal Attention

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # llm-from-scratch
-> My implementation an LLM,
-> made alongside [Build a Large Language Model (From Scratch) - book by Sebastian Raschka](https://www.manning.com/books/build-a-large-language-model-from-scratch)
+> Coding alongside reading - [Build a Large Language Model (From Scratch) - book by Sebastian Raschka](https://www.manning.com/books/build-a-large-language-model-from-scratch)
---
+++ src/architecture/attention/causal.py
@@ -0,0 +1,47 @@
+import torch
+from torch import nn, Tensor
+
+class CausalAttention(nn.Module):
+    """A causal attention Pytorch module with dropout"""
+
+    def __init__(
+            self,
+            d_in: int,
+            d_out: int,
+            context_length: int,
+            *,
+            dropout: float = 0.5,
+            kqv_bias: bool = False,
+    ) -> None:
+        super().__init__() 
+        self.d_in = d_in
+        self.d_out = d_out
+
+        self.W_k = nn.Linear(in_features=d_in, out_features=d_out, bias=kqv_bias)
+        self.W_q = nn.Linear(in_features=d_in, out_features=d_out, bias=kqv_bias)
+        self.W_v = nn.Linear(in_features=d_in, out_features=d_out, bias=kqv_bias)
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.mask: Tensor
+        self.register_buffer(
+            'mask',
+            torch.triu(torch.ones(context_length, context_length, dtype=torch.bool), diagonal=1),
+        )
+
+    def forward(self, x: Tensor):
+        keys: Tensor = self.W_k(x)
+        queries: Tensor = self.W_q(x)
+        values: Tensor = self.W_v(x)
+
+        attention_scores = queries @ keys.transpose(1, 2)
+        attention_scores_masked = attention_scores.masked_fill(
+            mask=self.mask[:x.shape[1], :x.shape[1]], 
+            value=-torch.inf,
+        )
+        attention_weights = torch.softmax(
+            attention_scores_masked / self.d_out**0.5,
+            dim=-1,
+        )
+
+        return attention_weights @ values
